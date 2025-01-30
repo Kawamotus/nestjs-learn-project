ef4b8585b3ed9016d588332dca3d460599946e3e
Kawamotus <gustavo.kawamoto@hotmail.com>
2025-01-30 14:31:17 -0300

    refactor: add @Injectable in interceptors

--- src/common/interceptors/error-handling.interceptor.ts
+++ src/common/interceptors/error-handling.interceptor.ts
@@ -2,11 +2,13 @@ import {
   BadRequestException,
   CallHandler,
   ExecutionContext,
+  Injectable,
   NestInterceptor,
   NotFoundException,
 } from '@nestjs/common';
 import { catchError, throwError } from 'rxjs';
 
+@Injectable()
 export class ErrorHandlingInterceptor implements NestInterceptor {
   async intercept(context: ExecutionContext, next: CallHandler<any>) {
     return next.handle().pipe(
